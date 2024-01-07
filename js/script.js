fcad8180d9a6a5d601a91d4f2c0fa9589b7166c5
Vinesh Gurram <vineshgurram@gmail.com>
2024-01-07 19:30:06 +0530

    some changes add

--- js/script.js
+++ js/script.js
@@ -30,19 +30,22 @@ function imageCategory(category) {
     onClickShowImage();
 }
 
-let natureCategoryButton = document.querySelector(".nature-category-btn");
 
+let natureCategoryButton = document.querySelector(".nature-category-btn");
 document.querySelector(".nature-category-btn").addEventListener("click", function () {
     imageCategory("Nature");
 });
+
 
 document.querySelector(".technology-category-btn").addEventListener("click", function () {
     imageCategory("Technology");
 });
 
+
 document.querySelector(".car-category-btn").addEventListener("click", function () {
     imageCategory("Cars");
 });
+
 
 document.querySelector(".all-category-btn").addEventListener("click", function () {
     allImages(imageCategories);
@@ -54,18 +57,11 @@ allImages(imageCategories);
 function onClickShowImage() {
     document.querySelectorAll(".image-box").forEach(function (el) {
         return el.addEventListener("click", function () {
-            var windHeight = window.outerHeight;
-            var windWidth = window.outerWidth;
-            // alert(windHeight)
-            // alert(windWidth)
             const imageSrc = this.querySelector("img").getAttribute("src");
             const imageAlt = this.querySelector("img").getAttribute("alt");
             const imageCaption = this.querySelector("img").getAttribute(`data-caption`);
             const imageTag = `<img src="${imageSrc}" alt="${imageAlt}" data-caption="${imageCaption}" loading="lazy"/>`
             document.querySelector(".modal-img").innerHTML = imageTag;
-            document.querySelector(".modal-img img").style.width= "70%";
-            // document.querySelector(".modal-img img").style.height= `${windHeight*(70/100)}px`;
-            // document.querySelector(".modal-caption").innerHTML = imageCaption;
             document.querySelector(".modal-caption").innerHTML = imageAlt;
             document.querySelector(".overlay").classList.add("active");
             document.querySelector(".modal").classList.add("active");
@@ -75,19 +71,30 @@ function onClickShowImage() {
 }
 
 
-document.querySelector(".close-btn,.overlay").addEventListener("click", function () {
+document.querySelector(".overlay").addEventListener("click", function () {
     document.querySelector(".overlay").classList.remove("active");
     document.querySelector(".modal").classList.remove("active");
+    document.querySelector(".upload-form").classList.remove("active");
 });
 
+
+document.querySelector(".close-btn").addEventListener("click", function () {
+    document.querySelector(".overlay").classList.remove("active");
+    document.querySelector(".modal").classList.remove("active");
+    document.querySelector(".upload-form").classList.remove("active");
+});
+
+
 let imageBoxLength = document.querySelectorAll(".image-box").length;
-console.log(imageBoxLength);
+// console.log(imageBoxLength);
 document.querySelector("#form").addEventListener("submit", function (event) {
     event.preventDefault();
     uploadImage()
 })
 
+
 // uploadImage()
+
 
 function uploadImage() {
     let url = document.querySelector("#url").value;
@@ -114,35 +121,17 @@ function uploadImage() {
 
 
 document.querySelector(".upload-img").addEventListener("click", function () {
-    document.querySelector(".overlay-1").classList.add("active");
+    document.querySelector(".overlay").classList.add("active");
     document.querySelector(".upload-form").classList.add("active");
+});
+
+
+let buttons = document.querySelectorAll(".category-btn .btn");
+document.querySelectorAll(".category-btn .btn").forEach(function(element){
+    element.addEventListener("click",function(event){
+        buttons.forEach(btns=>{
+            btns.classList.remove("active");
+        })
+        element.classList.add("active");
+    })
 })
-
-document.querySelector(".overlay-1").addEventListener("click", function () {
-    this.classList.remove("active");
-    document.querySelector(".upload-form").classList.remove("active");
-})
-
-var windHeight = window.outerHeight;
-var windWidth = window.outerWidth;
-alert(windHeight)
-alert(windWidth)
-
-
-// let imageGrid = imageCategories.filter(function(category){
-//     return category.images.filter(function(image,i){
-//         return image < 1 ? `<img src="${image.src}" alt="${image.alt}" data-caption="${image.caption}" loading="lazy"/>`: ""
-//     })
-// });
-
-// console.log(imageGrid)
-
-// let imageGrid = imageCategories.map(function(category) {
-//     if(category.images && category.images.length > 0){
-//         return `<img src="${category.images[0].src}" alt="${category.images[0].alt} loading="lazy">`
-//     }
-//     else{
-//         return ""
-//     }
-// }).join("");
-// document.querySelector(".all-category .grid").innerHTML = imageGrid;
